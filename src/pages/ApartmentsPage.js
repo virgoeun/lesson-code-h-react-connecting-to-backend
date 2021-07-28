0fce7843adb94d8444716b5520847b3c5a7970e5
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-28 11:59:36 +0200

    Example 2

--- src/pages/ApartmentsPage.js
+++ src/pages/ApartmentsPage.js
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";  // <== IMPORT useEffect
+import axios from "axios";                   // <== IMPORT axios
 
 function ApartmentsPage() {
   const [apartments, setApartments] = useState([]);
 
+
+  useEffect(() => {                     // <== ADD THE EFFECT
+    axios
+      .get("https://ironbnb-m3.herokuapp.com/apartments")
+      .then((response) => {
+        console.log("response.data", response.data);
+        setApartments(response.data);
+      });
+  }, []); // <--  [] means: Run the effect only once, after initial render
+
+  
   return (
     <div>
       <h3>List of apartments</h3>
+
+      {/*  👇  ADD 👇  */}
+      {apartments.map((apartment) => (
+        <div key={apartment._id} className="card">
+          <img src={apartment.img} alt="apartment" />
+          <h3>{apartment.title}</h3>
+          <p>Price: {apartment.pricePerDay}</p>
+        </div>
+      ))}
     </div>
   );
 }
